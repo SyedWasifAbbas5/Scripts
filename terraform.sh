dc8572616162dfb2ce5956fee9f1760ba64d2e69
Syed Wasif Abbas <124575436+SyedWasifAbbas5@users.noreply.github.com>
2023-08-07 00:58:26 +0500

    Terraform New version

--- terraform.sh
+++ terraform.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install unzip
 
-wget https://releases.hashicorp.com/terraform/1.0.7/terraform_1.0.7_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/1.5.4/terraform_1.5.4_linux_amd64.zip
 
 unzip terraform_1.0.7_linux_amd64.zip
 
