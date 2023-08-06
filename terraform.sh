2c0d17472e63b75cc137078a2dd5de13439a90d6
Syed Wasif Abbas <124575436+SyedWasifAbbas5@users.noreply.github.com>
2023-08-07 01:00:34 +0500

    Updated Code

--- terraform.sh
+++ terraform.sh
@@ -4,7 +4,7 @@ sudo apt-get install unzip
 
 wget https://releases.hashicorp.com/terraform/1.5.4/terraform_1.5.4_linux_amd64.zip
 
-unzip terraform_1.0.7_linux_amd64.zip
+unzip terraform_1.5.4_linux_amd64.zip
 
 sudo mv terraform /usr/local/bin/
 
